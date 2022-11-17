965fada5ebf71a5e85c8b882ada520b221cf36cb
Julio Souza <Juliocsouzas91@gmail.com>
2022-11-16 21:45:39 -0300

    Foi-se definido a frequência de amostragem do sinal de 440 hz para 1000 hz, na qual foi ilustrado os pontos adquiridos, ilustrando que o sinal está irreconhecível

--- sample_rate_songs/continuously_signal.py
+++ sample_rate_songs/continuously_signal.py
@@ -1,12 +1,12 @@
 #Define the images parameters
 import matplotlib
 import numpy as np 
-def signal_simulation(fc, fs, tempo_amostrando,phase=0):
+def signal_simulation(fc, fs, tempo_amostrando, phase=0):
         n_samples = np.arange(0,fs*tempo_amostrando, 1)
         sinal_senoidal = np.zeros(n_samples.size)
-        
+
         for aux1 in fc:
-                sinal_senoidal += np.sin(2*np.pi*aux1/fs*n_samples + phase)
+                sinal_senoidal += np.sin(2*np.pi*(aux1/fs)*n_samples + phase)
         t = (1/fs)*n_samples       
         sinal_senoidal = np.round(sinal_senoidal,20) 
         return t,sinal_senoidal
